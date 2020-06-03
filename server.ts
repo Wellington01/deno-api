a09e9bab7a20ad6cc59d48dd8d6be0b2433e1151
Wellington <wellington.c.v.santana@gmail.com>
2020-06-03 11:00:08 -0300

    alter server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./router.ts";
+import { PORT } from './config.ts';
 
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen("localhost:3333");
+console.log(`Running in localhost:${PORT}`);
+
+await app.listen(`localhost:${PORT}`);
